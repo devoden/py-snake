56c544ecda941b51d848134de7267b24ed56672e
devoden <devohutsdv@gmail.com>
2021-09-15 23:20:58 +0300

    Init

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+
+pygame.init()
+
+width = 800
+height = 600
+display = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Snake')
+
+green = (0, 255, 0)
+black = (0, 0, 0)
+
+segment_size = 20
+
+
+display.fill(green)
+
+pygame.draw.rect(display, black, [width // 2, height // 2, segment_size, segment_size])
+
+pygame.display.flip()
